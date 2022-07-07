11bfd392ac737783a3e4a2e7fffc68b54192f7dd
Javier Herreros <javi3rhr@gmail.com>
2022-07-07 13:48:11 +0200

    casi final

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,28 @@ const toggleColors = document.getElementById('toggle-colors');
 // Todas las variables del css
 const rootStyles = document.documentElement.style;
 
+const flagsElement = document.getElementById('flags');
+
+const textsToChange = document.querySelectorAll("[data-section]");
+
+const changeLanguage = async (language) => {
+    const requestJson = await fetch(`./languages/${language}.json`);
+    const texts = await requestJson.json(); // json to object
+
+    for(const textToChange of textsToChange){
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+
+        textToChange.innerHTML = texts[section][value];
+    }
+}
+
+// Recoje identificador para saber el json que se va a cargar
+flagsElement.addEventListener('click', (e) => {
+    changeLanguage(e.target.parentElement.dataset.language);
+    // console.log(e.target.parentElement.dataset.language);
+});
+
 toggleTheme.addEventListener('click', () => {
     // toggle -> Si no tiene la clase se la pone, y si la tiene se la quita
     document.body.classList.toggle('dark');
